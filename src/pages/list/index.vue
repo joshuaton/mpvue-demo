34f31dcd7742747c9c6d5c0b97e39ffb703c4937
junshao <junshao@tencent.com>
2018-07-10 13:52:34 +0800

    【新增】列表页

---
+++ src/pages/list/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<div v-for="(item, index) in lists" :key="item.id" class="item">
+			<img :src="item.picUrl" class="item-pic-url"></img>
+			<div class="item-title-desc">
+				<div class="item-title">{{item.title}}</div>
+				<div class="item-desc">{{item.desc}}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default{
+	data () {
+		return {
+			lists: [
+				{
+					id : '1',
+					title: 'title',
+					desc: 'desc',
+					picUrl: 'http://mat1.gtimg.com/www/qq2018/imgs/qq_logo_2018x2.png'
+				},
+				{
+					id : '2',
+					title: 'title',
+					desc: 'desc',
+					picUrl: 'http://mat1.gtimg.com/www/qq2018/imgs/qq_logo_2018x2.png'
+				},
+				{
+					id : '3',
+					title: 'title',
+					desc: 'desc',
+					picUrl: 'http://mat1.gtimg.com/www/qq2018/imgs/qq_logo_2018x2.png'
+				}
+			]
+		}
+	}
+}
+</script>
+
+<style>
+.item{
+	height: 200rpx;
+	display: flex;
+}
+.item-pic-url{
+	width: 180rpx;
+	height: 180rpx;
+	margin: 10rpx;
+}
+.item-title-desc{
+	display: flex;
+	flex-direction: column;
+	flex: auto;
+	justify-content: center;
+}
+
+
+</style>
